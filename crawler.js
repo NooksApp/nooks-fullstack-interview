fc7e6fb1febfc0d40ab94e0530a1b1bcb941ca4d
Daniel <dan9lee@stanford.edu>
2023-03-23 17:47:10 -0700

    nicer function

--- crawler.js
+++ crawler.js
@@ -1,8 +1,18 @@
 import axios from "axios";
-
 const SERVER_URL = "https://nooks-takehome-server.ngrok.io";
 
-// This API request gets the neighbors of node A, or fails.
-const response = await axios.get(`${SERVER_URL}/get_neighbors?node=A`);
+// This API request queries a simple graph data structure
+// The graph is simple. Edges are undirected & unweighted
+// It returns the neighbors of 'queryNode' in a list
+const fetchNeighbors = async (queryNode) => {
+  const response = await axios.get(
+    `${SERVER_URL}/get_neighbors?node=${queryNode}`
+  );
+  console.log(response.data);
+  return response.data;
+};
 
+fetchNeighbors("A");
+
+// queries a graph data structure
 // return a list of all the nodes in the graph, sorted by their distance from the root node A
